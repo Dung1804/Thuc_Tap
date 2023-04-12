06c42c73f6fd2a0a1d8dd052c82da50ba1c63974
Hoang Dung <hoanganhdung18042002@gmail.com>
2023-04-12 22:17:40 +0700

    push project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Ban_Rau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
